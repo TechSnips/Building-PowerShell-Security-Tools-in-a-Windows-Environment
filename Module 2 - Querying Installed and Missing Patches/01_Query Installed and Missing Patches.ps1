267a986b9c9be4651a037b71bdb0bdc878841ba9
Adam Bertram <adbertram@gmail.com>
2019-02-13 15:45:37 -0600

    updates

--- Module 2 - Querying Installed and Missing Patches/01_Query Installed and Missing Patches.ps1
+++ Module 2 - Querying Installed and Missing Patches/01_Query Installed and Missing Patches.ps1
@@ -1,75 +1,66 @@
-#region Find Updates
-$UpdateObjectSession = New-Object -ComObject 'Microsoft.Update.Session'
-$UpdateSearcher      = $UpdateObjectSession.CreateUpdateSearcher()
+## Scenario: Query updates different ways on a local and remote computer
 
-$Updates = $UpdateSearcher.Search($null)
-$Updates.Updates | Select-Object Title, Description, RebootRequired, IsDownloaded, IsHidden
+#region Explain the entire process without the function all in one go in it's simplest form
+$updateSession  = New-Object -ComObject 'Microsoft.Update.Session'
+$updateSearcher = $updateSession.CreateUpdateSearcher()
+$query = 'IsInstalled=0'
+$updates = ($updateSearcher.Search($query))
+
+## Show there's not much good output here
+$updates
+
+## Need to drill down into the updates property
+$updates.Updates
+
+## Limit to only interesting data
+$updates.Updates | Select-Object Title, LastDeploymentChangeTime, Description, RebootRequired, IsDownloaded, IsHidden
 #endregion
 
-#region Find Installed Updates
-$Updates = $UpdateSearcher.Search('IsInstalled=1')
-$Updates.Updates | Select-Object Title, Description, RebootRequired, IsDownloaded, IsHidden
-#endregion
+#region Hit another use case or two trying not to repeat much from above
 
-#region Find Updates Required a Reboot
+## Find Updates Required a Reboot
 $Updates = $UpdateSearcher.Search('RebootRequired=1')
 $Updates.Updates | Select-Object Title, Description, RebootRequired, IsDownloaded, IsHidden
-#endregion
 
-#region Find Hidden Updates
-$Updates = $UpdateSearcher.Search('IsHidden=1')
-$Updates.Updates | Select-Object Title, Description, RebootRequired, IsDownloaded, IsHidden
-#endregion
-
-#region Find Assigned Updates
-# These are updates that are intended for deployment by Windows Automatic Updates
-$Updates = $UpdateSearcher.Search('IsAssigned=1')
-$Updates.Updates | Select-Object Title, Description, RebootRequired, IsDownloaded, IsHidden
-#endregion
-
-#region Multiple Conditions
+## Multiple Conditions
 $Updates = $UpdateSearcher.Search('IsInstalled=0 AND RebootRequired=1')
 $Updates.Updates | Select-Object Title, Description, RebootRequired, IsDownloaded, IsHidden
+
 #endregion
 
 #region Search by Category
 $UpdateObjectSearcher = New-Object -ComObject 'Microsoft.Update.Searcher'
 $InstalledUpdates     = $UpdateObjectSearcher.Search("IsInstalled=1")
+$InstalledUpdates.Updates | % {$_.Categories | % {$_.Name}}
 
-$InstalledUpdates.Updates | Foreach-Object {
-    $Result = $_.Categories | Where-Object Name -EQ 'Definition Updates'
-
-    If ($Result) {
-        $_
-    }
-} | Select-Object Title, LastDeploymentChangeTime
+$InstalledUpdates.Updates | Where-Object { 'Security Updates' -in ($_.Categories | foreach { $_.Name }) } | Select-Object Title, LastDeploymentChangeTime
 #endregion
+
+## Other query options
+## RebootRequired=1, IsHidden=1, IsAssigned=1, IsInstalled=0 AND RebootRequired=1
 
 #region Get Updates on a Remote Computer (PSRemoting)
 $scriptblock = {
-    $UpdateObjectSession = New-Object -ComObject 'Microsoft.Update.Session'
-    $UpdateSearcher      = $UpdateObjectSession.CreateUpdateSearcher()
+	$UpdateObjectSession = New-Object -ComObject 'Microsoft.Update.Session'
+	$UpdateSearcher      = $UpdateObjectSession.CreateUpdateSearcher()
 
-    $Updates = $UpdateSearcher.Search($null)
-    $Updates.Updates | Select-Object Title, Description, RebootRequired, IsDownloaded, IsHidden
+	$Updates = $UpdateSearcher.Search($null)
+	$Updates.Updates | Select-Object Title, Description, RebootRequired, IsDownloaded, IsHidden
 }
-
 Invoke-Command -ComputerName 'DC' -ScriptBlock $scriptblock
 #endregion
 
-#region Remotely Trigger Update Detection
+#region Remotely Trigger Update Detection (wuauclt /detectnow)
 $scriptblock = {
-    $AutoUpdate = New-Object -ComObject 'Microsoft.Update.AutoUpdate'
-    $AutoUpdate.DetectNow()
+	$AutoUpdate = New-Object -ComObject 'Microsoft.Update.AutoUpdate'
+	$AutoUpdate.DetectNow()
 }
-
 Invoke-Command -ComputerName 'DC' -ScriptBlock $scriptblock
 
 $scriptblock = {
-    $AutoUpdate = New-Object -ComObject 'Microsoft.Update.AutoUpdate'
-    $AutoUpdate.Results
+	$AutoUpdate = New-Object -ComObject 'Microsoft.Update.AutoUpdate'
+	$AutoUpdate.Results
 }
-
 Invoke-Command -ComputerName 'DC' -ScriptBlock $scriptblock
 #endregion
 
@@ -77,81 +68,108 @@ Invoke-Command -ComputerName 'DC' -ScriptBlock $scriptblock
 # Microsoft Updates (normally the default) is MS product updates and everything in Windows Updates
 # Windows Updates are Service Packs and core dupates but not product updates
 $serviceManager = New-Object -Com 'Microsoft.Update.ServiceManager'
-$serviceManager.Services | Select-Object Name, ISManaged, IsDefaultAUService,ServiceUrl
+$serviceManager.Services | Select-Object Name, ISManaged, IsDefaultAUService, ServiceUrl
 #endregion
 
-#region Get-WindowsUpdate
+#region Wrap it all up into a function
+
 Function Get-WindowsUpdate {
-    [OutputType([System.Management.Automation.PSObject])]
-    [CmdletBinding()]
+	<#
+        .SYNOPSIS
+            This function queries a local or remote computers for Windows patches.
+        .PARAMETER Installed
+            A switch parameter to limit updates returned to only installed updates.
+        .PARAMETER Hidden
+            A switch parameter to limit updates returned to only hidden updates.
+        .PARAMETER Assigned
+            A switch parameter to limit updates returned to only assigned updates.
+        .PARAMETER RebootRequired
+            A switch parameter to limit updates returned to only those that will require a reboot after installation.
+        .PARAMETER ComputerName
+            A string parameter representing a remote computer to query. If this is not provided, this function will query
+            updates on the local computer.
+    
+        .EXAMPLE
+            PS> Get-WindowsUpdate -Installed -Hidden -ComputerName SRV1
 
-    Param (
-        [Switch]$Installed,
-        [Switch]$Hidden,
-        [Switch]$Assigned,
-        [Switch]$RebootRequired,
+            This example will attempt to find all updates that are installed and hidden on the remote SRV1 computer.
+    
+    #>
+	[OutputType([pscustomobject])]
+	[CmdletBinding()]
 
-        [Parameter(ValueFromPipeline)]
-        [ValidateNotNullOrEmpty()]
-        [String]$ComputerName,
+	Param (
+		[Switch]$Installed,
+		[Switch]$Hidden,
+		[Switch]$Assigned,
+		[Switch]$RebootRequired,
 
-        [Switch]$PassThru
-    )
+		[Parameter(ValueFromPipeline)]
+		[Alias('Name')]
+		[ValidateNotNullOrEmpty()]
+		[String]$ComputerName
+	)
 
-    Begin {
-        $conversion = @{
-            Installed      = 'IsInstalled'
-            Hidden         = 'IsHidden'
-            Assigned       = 'IsAssigned'
-            RebootRequired = 'RebootRequired'
-        }
+	Begin {
+		## Create a hashtable to easily "convert" the function paramters to query parts.
+		$paramToQueryMap = @{
+			Installed      = 'IsInstalled'
+			Hidden         = 'IsHidden'
+			Assigned       = 'IsAssigned'
+			RebootRequired = 'RebootRequired'
+		}
 
-        $Query = @()
-    }
+		$query = @()
+	}
 
-    Process {
-        $conversion.GetEnumerator() | Foreach-Object {
-            $condition = Get-Variable $_.Key -Scope Local
+	Process {
 
-            If ($condition -And $condition.Value -NE $false) {
-                $Query += '{0}={1}' -f $conversion[$condition.Name], [Int][Bool]$condition.Value
-            }
-        }
+		## Build the query string
+		$paramToQueryMap.GetEnumerator() | Foreach-Object {
+			if ($PSBoundParameters.ContainsKey($_.Name)) {
+				$query += '{0}=1' -f $paramToQueryMap[$_.Name]
+			}
+		}
 
-        $Query = $Query -Join ' AND '
+		$query = $query -Join ' AND '
 
-        Try {
-            $scriptBlock = {
-                Param (
-                    $Query,
-                    $PassThru
-                )
+		try {
 
-                Write-Verbose "Query is '$Query'"
-                Write-Verbose "PassThru is '$PassThru'"
+			## Create the scriptblock we'll use to pass to the remote computer or run locally
+			$scriptBlock = {
+				param (
+					$Query,
+					$PassThru
+				)
 
-                $updateSession  = New-Object -ComObject 'Microsoft.Update.Session'
-                $updateSearcher = $updateSession.CreateUpdateSearcher()
+				Write-Verbose "Query is '$Query'"
+				Write-Verbose "PassThru is '$PassThru'"
 
-                If ($updates = ($updateSearcher.Search($Query))) {
-                    If ($PassThru) {
-                        $updates.Updates
-                    } Else {
-                        $updates.Updates | Select-Object Title, LastDeploymentChangeTime
-                    }
-                }
-            }
+				$updateSession  = New-Object -ComObject 'Microsoft.Update.Session'
+				$updateSearcher = $updateSession.CreateUpdateSearcher()
 
-            If ($ComputerName) {
-                 Write-Verbose "Query is '$Query'"
-                Invoke-Command -ComputerName $ComputerName -ScriptBlock $scriptBlock -ArgumentList $Query, $PassThru |
-                    Select-Object PSRemoteComputer, Title, LastDeploymentChangeTime
-            } Else {
-                & $scriptBlock $Query $PassThru
-            }
-        } Catch {
-            Throw $_.Exception.Message
-        }
-    }
+				if ($updates = ($updateSearcher.Search($Query))) {
+					$updates.Updates | Select-Object Title, LastDeploymentChangeTime
+				}
+			}
+
+			if ($PSBoundParameters.ContainsKey('ComputerName')) {
+				Write-Verbose "Query is '$Query'"
+				Invoke-Command -ComputerName $ComputerName -ScriptBlock $scriptBlock -ArgumentList $Query |
+					Select-Object PSComputerName, Title, LastDeploymentChangeTime
+			} else {
+				& $scriptBlock $Query
+			}
+		} catch {
+			throw $_.Exception.Message
+		}
+	}
 }
 #endregion
+
+## Function demonstration
+Get-WindowsUpdate
+Get-WindowsUpdate -ComputerName DC
+Get-WindowsUpdate -ComputerName DC -Installed
+
+Get-AdComputer | Get-WindowsUpdate
