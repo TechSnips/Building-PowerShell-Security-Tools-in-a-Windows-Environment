391ca6180ed455032a1e923004c32048d21dade0
Adam <adam@listekconsulting.com>
2019-02-25 01:08:58 -0600

    Further Module 4 Updates

--- Module 4 - Managing Sensitive Information Securely/02_Saving Sensitive Information to Disk.ps1
+++ Module 4 - Managing Sensitive Information Securely/02_Saving Sensitive Information to Disk.ps1
@@ -68,3 +68,56 @@ $Protected | Get-CMSMessage
 # Read out the content
 $Protected | Unprotect-CmsMessage
 #endregion
+
+#region Import/Export CLIXML
+# Encrypt Data
+$Path = "$($Env:Home)\Desktop\Credential.xml"
+Get-Credential | Export-Clixml -Path $Path
+
+# Show Encrypted Values
+Get-Content -Path $Path
+
+# Get Credentials
+$Credential = Import-CliXml -Path $Path
+
+$Credential.GetNetworkCredential().Password
+#endregion
+
+#region Create Secure Strings
+$Path = "$($Env:Home)\Desktop\SecureString.txt"
+
+# Save Secure String to a File
+'MyAPIKey' |  ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString | Set-Content -Path $Path
+
+# Show Value is Encrypted
+Get-Content -Path $Path
+
+# Retrieve Saved Secure String and Save to a Variable
+$SecureString = Get-Content  -Path $Path | ConvertTo-SecureString
+
+[Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR((($SecureString))))
+#endregion
+
+#region Using AES Key
+$AESKeyPath = "$($Env:HOME)\Desktop\aes.key"
+$PassPath   = "$($Env:HOME)\Desktop\password.txt"
+
+# Use RNGCryptoServiceProvider instead of Get-Random (better random number generator)
+# AES is a subset of Rijndael
+# AES comes in 3 block sizes of 128, 192 and 256
+# AES superseded DES
+# Generally let the default encryption do the work for you, setting a fixed Salt (hashing) or IV (encrypting) means every other key you encrypt will be decrypable if one is broken. It's best to let each be unique so that it's more secure.
+$Key = [System.Byte[]]::New(32)
+[Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($Key)
+$Key | Out-File $AESKeyPath
+
+# Get the Password and encrypt with the AES Key
+(Get-Credential).Password | ConvertFrom-SecureString -Key (Get-Content $AESKeyPath) | Set-Content -Path $PassPath
+Get-Content -Path $PassPath | ConvertTo-SecureString -Key (Get-Content -Path $AESKeyPath)
+
+# Retrieve Password
+$SecurePassword = Get-Content $PassPath | ConvertTo-SecureString -Key (Get-Content -Path $AESKeyPath)
+$BSTR           = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword)
+$PlainPassword  = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+$PlainPassword
+#endregion
--- Module 4 - Managing Sensitive Information Securely/05_Mini-Project Building a Password Management Tool.ps1
+++ Module 4 - Managing Sensitive Information Securely/05_Mini-Project Building a Password Management Tool.ps1
@@ -3,3 +3,182 @@
 # 2) Get-DocumentProtectionCertificate
 # 3) Protect-Item
 # 4) Unprotect-Item
+
+#region New-DocumentProtectionCertficate
+Function New-DocumentProtectionCertficate {
+	[OutputType([System.Security.Cryptography.X509Certificates.X509Certificate2])]
+    [CmdletBinding()]
+
+    Param (
+        [Parameter()]
+        $CertStoreLocation = 'Cert:\CurrentUser\My',
+
+        [Parameter()]
+        $Subject = "PSDocumentProtection",
+
+        [Parameter()]
+        $Years = (Get-Date).AddYears(1000),
+
+        [Parameter()]
+        $KeyLength = 2048,
+
+        [Switch]$PassThru
+    )
+
+    Process {
+        $CertParams = @{
+            'Subject'           = $Subject
+            'CertStoreLocation' = $CertStoreLocation
+            'KeyUsage'          = @('KeyEncipherment', 'DataEncipherment')
+            'Type'              = 'DocumentEncryptionCert'
+            'KeySpec'           = 'KeyExchange'
+            'KeyExportPolicy'   = 'Exportable'
+            'KeyLength'         = 2048
+            'KeyAlgorithm'      = 'RSA'
+            'NotAfter'          = $Years
+            'ErrorAction'       = 'Stop'
+        }
+
+        Try {
+            $Result = New-SelfSignedCertificate @CertParams
+        } Catch {
+            $Error[0]
+        }
+
+        If ($PassThru) {
+            $Result
+        }
+    }
+}
+#endregion
+
+#region Get-DocumentProtectionCertficate
+Function Get-DocumentProtectionCertficate {
+    [OutputType([System.Security.Cryptography.X509Certificates.X509Certificate2])]
+    [CmdletBinding()]
+
+    Param (
+        [Parameter()]
+        $CertLocation = 'Cert:\CurrentUser\My',
+
+        [Parameter()]
+        $Subject = "CN=PSDocumentProtection",
+
+        [Parameter()]
+        $Thumbprint
+    )
+
+    Process {
+        If ($Thumbprint) {
+            $WhereCondition = { $_.Thumbprint -EQ $Thumbprint }
+        } Else  {
+            $WhereCondition = { $_.Subject -EQ $Subject }
+        }
+
+        Try {
+            Get-ChildItem -Path $CertLocation -ErrorAction 'Stop' | Where-Object $WhereCondition | Select-Object *
+        } Catch {
+            $Error[0]
+        }
+    }
+}
+#endregion
+
+#region Protect-Item
+Function Protect-Item {
+	[OutputType([System.Management.Automation.PSObject])]
+    [CmdletBinding()]
+
+    Param (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        $Content,
+
+        [Parameter(Mandatory)]
+        $Path,
+
+        [Parameter()]
+        [ValidateSet("CMS","DPAPI","AES")]
+        $Method = "DPAPI",
+
+        [Parameter()]
+        $Thumbprint,
+
+        [Parameter()]
+        $KeyPath,
+
+        [Switch]$Force
+    )
+
+    Process {
+        Switch ($Method) {
+            "CMS" {
+                $Content | Protect-CmsMessage -To ($Thumbprint -Replace '(..(?!$))','$1 ') -OutFile $Path
+
+                Break
+            }
+            "DPAPI" {
+                $Content | ConvertTo-SecureString -AsPlainText -Force | Export-CliXML -Path $Path
+
+                Break
+            }
+            "AES" {
+                If (-Not (Test-Path $KeyPath) -And -Not $Force) {
+                    $Key = [System.Byte[]]::New(32)
+                    [Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($Key)
+                    $Key | Out-File $KeyPath
+                }
+
+                $Content | ConvertFrom-SecureString -Key (Get-Content $KeyPath) | Set-Content -Path $Path
+
+                Break
+            }
+        }
+    }
+}
+#endregion
+
+#region Unprotect-Item
+Function Unprotect-Item {
+    [OutputType([System.Management.Automation.PSObject])]
+    [CmdletBinding()]
+
+    Param (
+        [Parameter(Mandatory)]
+        $Path,
+
+        [Parameter()]
+        [ValidateSet("CMS","DPAPI","AES")]
+        $Method = "DPAPI",
+
+        $Thumbprint,
+
+        $KeyPath
+    )
+
+    Process {
+        Switch ($Method) {
+            "CMS" {
+                $Content | Protect-CmsMessage -To ($Thumbprint -Replace '(..(?!$))', '$1 ') -OutFile $Path
+
+                Break
+            }
+            "DPAPI" {
+                $SecureString = Get-Content -Path $Path | ConvertTo-SecureString
+
+                [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR((($SecureString))))
+
+                Break
+            }
+            "AES" {
+                $SecurePassword = Get-Content $Path | ConvertTo-SecureString -Key (Get-Content -Path $KeyPath)
+                $BSTR           = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword)
+                $PlainContent   = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+                $PlainContent
+
+                Break
+            }
+        }
+    }
+}
+#endregion
